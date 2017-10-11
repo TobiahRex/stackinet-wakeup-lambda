26c71fad059c94947dec24c0da83bc7745dc0d69
Lakshman Diwaakar <diwaakartg@gmail.com>
2017-10-11 17:41:23 +0900

    added lambda.invoke to invoke lambda using SDK

--- handler.js
+++ handler.js
@@ -1,43 +1,50 @@
 if (!global._babelPolyfill) require('babel-polyfill');
 
-import axios from 'axios';
+// import axios from 'axios';
+import { Promise as bbPromise } from 'bluebird';
+import AWS from 'aws-sdk';
+
+const lambda = new AWS.Lambda({
+    region: 'ap-northeast-1'
+});
 
 module.exports.wakeup = (event, context, callback) => {
   console.log('Calling Wakeup Lambda.');
 
   const wakeupOptions = {
     nj2jp: {
-      endpoint: process.env.NJ2JP_WAKEUP,
-      option: {
-        query: 'query{FindProductById(_id: \"Test\") {_id, product { mainTitle title flavor price sku sizes nicotine_strengths routeTag vendor blurb images{ purpose url } dates {added_to_store  removed_from_store }}}}',
-      },
+      functionName: process.env.NJ2JP_WAKEUP,
+      payload: '{ "body" : {"query":"query{PopularProducts(qty: 6) {_id, docId, slug, images {purpose url}, title{en ja}, completedCheckouts}}"}}',
     },
     lonesmoke: {
-      endpoint: process.env.LONESMOKE_WAKEUP,
-      option: {
-        userEmail: 'wakeup@stakinet.com',
-      },
+      functionName: process.env.LONESMOKE_WAKEUP,
+      payload: '{ "body" : {"userEmail":"wakeup@stakinet.com"}}',
     },
   };
 
   const asyncConcurrentWakeup = async (wakeupObj) => {
     const keys = Object.keys(wakeupObj);
 
-    const callLambda = async (endpoint, options) => await axios.post(endpoint, options);
+    // const callLambda = async (endpoint, payload) => await axios.post(endpoint, options);
+    const callLambda = async (functionName, payload) => await bbPromise.fromCallback(cb => lambda.invoke({
+       FunctionName: functionName,
+       InvocationType: 'RequestResponse',
+       Payload: payload
+    }, cb));
 
     const results = await Promise.all([
-      callLambda(wakeupObj[keys[0]].endpoint, wakeupObj[keys[0]].option),
-      callLambda(wakeupObj[keys[1]].endpoint, wakeupObj[keys[1]].option),
+      callLambda(wakeupObj[keys[0]].functionName, wakeupObj[keys[0]].payload),
+      callLambda(wakeupObj[keys[1]].functionName, wakeupObj[keys[1]].payload),
     ])
     .catch((error) => {
       console.error('ERROR: \n', error);
-      callback(null, error);
+      callback(error);
     });
-    results.forEach(({ status, config: { url } }, i) => {
+    // console.log('results: ', results);
+    results.forEach(({ StatusCode }, i) => {
       console.log(`
         REQUEST #${i + 1}:
-        URL: ${url}
-        RESPONSE: ${status}
+        RESPONSE: ${StatusCode}
       `);
     });
   }
